e28c44700565812e60f0d17b0eb7603cc077bcfc
matheusOliv23 <matheushenri1996@gmail.com>
2023-01-04 06:39:26 -0300

    feat: read all files with primises

---
+++ Projeto/functions.js
@@ -0,0 +1,41 @@
+const fs = require('fs')
+const path = require('path')
+
+function lerDiretorio(caminho) {
+    return new Promise((resolve, reject) => {
+        try {
+            let arquivos = fs.readdirSync(caminho)
+            arquivos = arquivos.map(arquivo => path.join(caminho, arquivo))
+            resolve(arquivos)
+        } catch (e) {
+            reject(e)
+        }
+    })
+}
+
+function lerArquivo(caminho) {
+    return new Promise((resolve, reject) => {
+        try {
+            const conteudo = fs.readFileSync(caminho, { encoding: 'utf-8' })
+            resolve(conteudo.toString())
+        } catch (error) {
+            reject(error)
+        }
+
+    })
+}
+
+function lerArquivos(caminhos) {
+    return Promise.all(caminhos.map(caminho => lerArquivo(caminho)))
+}
+
+function elementosTerminadosCom(arr, terminadoCom) {
+    return arr.filter(el => el.endsWith(terminadoCom))
+}
+
+module.exports = {
+    lerDiretorio,
+    lerArquivo,
+    lerArquivos,
+    elementosTerminadosCom
+}
