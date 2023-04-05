93b3b560e51ec0f87a362d37c25aeb3f6175a816
matheusOliv23 <matheushenri1996@gmail.com>
2023-04-05 18:35:52 -0300

    feat: add exercises

--- Projeto/index.js
+++ Projeto/index.js
@@ -6,18 +6,35 @@ const caminho = path.join(__dirname, '..', 'dados', 'legendas')
 const simbolos = ['.', '?', '-', ',', '"', '♪', '_', '<i>', '</i>', '\r', '[', ']', '(', ')']
 
 
-fn.lerDiretorio(caminho)
-    .then(arquivos => fn.elementosTerminadosCom(arquivos, '.srt'))
-    .then(arquivosSTR => fn.lerArquivos(arquivosSTR))
-    .then(fn.mesclarElementos)
-    .then(fn.separarTextoPor('\n'))
-    .then(linhas => fn.removerSeVazio(linhas))
-    .then(linhas => fn.removerSeTiver('-->')(linhas))
-    .then(linhas => fn.removerSeApenasNumeros(linhas))
-    .then(fn.removerSimbolos(simbolos))
-    .then(fn.mesclarElementos)
-    .then(fn.separarTextoPor(' '))
-    .then(fn.removerSeVazio)
-    .then(fn.agruparElementos)
-    .then(fn.ordenarPorAtributoNumerico('qtde', 'desc'))
-    .then(console.log)
+// fn.lerDiretorio(caminho)
+//     .then(arquivos => fn.elementosTerminadosCom(arquivos, '.srt'))
+//     .then(arquivosSTR => fn.lerArquivos(arquivosSTR))
+//     .then(fn.mesclarElementos)
+//     .then(fn.separarTextoPor('\n'))
+//     .then(linhas => fn.removerSeVazio(linhas))
+//     .then(linhas => fn.removerSeTiver('-->')(linhas))
+//     .then(linhas => fn.removerSeApenasNumeros(linhas))
+//     .then(fn.removerSimbolos(simbolos))
+//     .then(fn.mesclarElementos)
+//     .then(fn.separarTextoPor(' '))
+//     .then(fn.removerSeVazio)
+//     .then(fn.agruparElementos)
+//     .then(fn.ordenarPorAtributoNumerico('qtde', 'desc'))
+//     .then(console.log)
+
+const palavrasMaisUsadas = fn.composicao(
+    fn.lerDiretorio,
+    fn.elementosTerminadosCom('.srt'),
+    fn.lerArquivos,
+    fn.mesclarElementos,
+    fn.separarTextoPor('\n'),
+    fn.removerSeVazio,
+    fn.removerSeTiver('-->'),
+    fn.removerSeApenasNumeros,
+    fn.removerSimbolos,
+    fn.removerSeVazio,
+    fn.agruparElementos,
+    fn.ordenarPorAtributoNumerico('qtde', 'desc')
+)
+
+palavrasMaisUsadas(caminho).then(console.log)
